265e6de2992ecc320e0ef69461a65dfd6145c8bd
Aryna Zaiko <arishazaiko@gmail.com>
2020-06-14 14:51:34 +0300

    asset setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import './style.css';
+import Icon from './icon.png';
+
 const element = document.createElement('div');
 
 element.innerText = 'Hello world!';
 
 document.body.appendChild(element);
+
+element.classList.add('hello');
+
+const myIcon = new Image();
+
+myIcon.src = Icon;
+
+element.appendChild(myIcon);
+
+myIcon.classList.add('icon');
